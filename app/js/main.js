2c745ce8d74736e7cd876c119b5662228d368799
JimakPasha <Jimak@tut.by>
2020-12-24 07:51:45 +0300

    added about and results section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function () {
-  
+
+  $('.about-tabs__top-item').on('click', function(e){
+    e.preventDefault();
+    $('.about-tabs__top-item').removeClass('about-tabs__top-item--active');
+    $(this).addClass('about-tabs__top-item--active');
+
+    $('.about-tabs__content-item').removeClass('about-tabs__content-item--active');
+    $($(this).attr('href')).addClass('about-tabs__content-item--active');
+
+  })
+
 });
