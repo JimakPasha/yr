32f8b93b869c766bf8bae1c96c8746a0cccc5b2e
JimakPasha <Jimak@tut.by>
2020-12-25 21:16:39 +0300

    added connection section

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,7 @@ $(function () {
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt="arrow-prev"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="arrow-next"></button>',
+    fade: true,
     autoplay: true
   });
 
